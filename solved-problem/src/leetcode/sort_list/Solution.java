54199e808fa3c40011dc0585a0f1a61e5803d5b1
pbg0205 <pbg0205@naver.com>
2021-07-10 17:07:32 +0900

    [#1][leetcode] Sort List

---
+++ solved-problem/src/leetcode/sort_list/Solution.java
@@ -0,0 +1,61 @@
+package leetcode.sort_list;
+
+/**
+ * 실행 시간 : 9ms (41.9%)
+ * 메모리 사용 : 53.4MD (10.08%)
+ */
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            mid = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        mid.next = null;
+
+        return merge(sortList(head), sortList(slow));
+    }
+
+    private ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
