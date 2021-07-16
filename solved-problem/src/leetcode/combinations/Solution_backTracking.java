6628ac5ecf2f372f82bb03dda4fae6fed9b12968
pbg0205 <pbg0205@naver.com>
2021-07-16 12:46:58 +0900

    [#3][leetcode] Combinations:backtracking

---
+++ solved-problem/src/leetcode/combinations/Solution_backTracking.java
@@ -0,0 +1,34 @@
+package leetcode.combinations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution_backTracking {
+
+    boolean[] visited;
+    List<List<Integer>> result;
+    int m;
+
+    public List<List<Integer>> combine(int n, int k) {
+        result = new ArrayList<>();
+        visited = new boolean[n];
+
+        m = k;
+        backTracking(n, k, 1, new ArrayList<>());
+
+        return result;
+    }
+
+    private void backTracking(int n, int k, int start, List<Integer> list) {
+        if (list.size() == k) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            list.add(i);
+            backTracking(n, k, i + 1, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
