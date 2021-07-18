38cf190ad7b7a912a815bfba6b96788c42b056e0
pbg0205 <pbg0205@naver.com>
2021-07-19 00:22:41 +0900

    [#5][leetcode] Search a 2D Matrix II

---
+++ solved-problem/src/leetcode/search_a_2d_matrix_ii/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.search_a_2d_matrix_ii;
+
+/**
+ * - Runtime: 4 ms, faster than 100.00% of Java
+ * - Memory Usage: 44.6 MB, less than 67.32% of Java
+ */
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0){
+            return false;
+        }
+
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while ((row <= matrix.length - 1) && (col >= 0)) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+
+            if (matrix[row][col] > target) {
+                col -= 1;
+                continue;
+            }
+
+            if (matrix[row][col] < target) {
+                row += 1;
+            }
+        }
+        return false;
+    }
+}
