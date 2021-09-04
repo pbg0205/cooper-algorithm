6af7425d7d7b4edc0e370d2256c08e27942f0cf2
pbg0205 <pbg0205@naver.com>
2021-09-04 22:37:05 +0900

    [#20][cooper] subsets

---
+++ algorithm/src/main/java/solved_problem/leetcode/subsets/Solution.java
@@ -0,0 +1,30 @@
+package solved_problem.leetcode.subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    List<List<Integer>> output = new ArrayList();
+    int n, k;
+
+    public List<List<Integer>> subsets(int[] nums) {
+        n = nums.length;
+        for (k = 0; k < n + 1; ++k) {
+            backtrack(0, new ArrayList<>(), nums);
+        }
+        return output;
+    }
+
+    public void backtrack(int first, ArrayList<Integer> curr, int[] nums) {
+        // if the combination is done
+        if (curr.size() == k) {
+            output.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = first; i < n; ++i) {
+            curr.add(nums[i]);
+            backtrack(i + 1, curr, nums);
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
