aa18753c4576b453e27a15247d749ec1047d5f47
cooper <pbg0205@naver.com>
2023-01-15 16:34:48 +0900

    [baekjoon] 자기복제수_2028

---
+++ src/main/java/solved_problem/baekjoon/자기복제수_2028/Main.java
@@ -0,0 +1,42 @@
+package solved_problem.baekjoon.자기복제수_2028;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Integer> inputs = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            inputs.add(Integer.parseInt(scanner.nextLine()));
+        }
+
+        for (Integer input : inputs) {
+            if (isSelfReplicationNumber(input)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+    }
+
+    private static boolean isSelfReplicationNumber(int number) {
+        int numberSquare = number * number;
+
+        int numberDigit = (int) Math.log10(number) + 1;
+        int numberSquareDigit = (int) Math.log10(numberSquare) + 1;
+
+        int startIndex = numberSquareDigit - numberDigit;
+
+        String compareNumber = String.valueOf(numberSquare).substring(startIndex, numberSquareDigit);
+
+        return String.valueOf(number).equals(compareNumber);
+    }
+
+}
