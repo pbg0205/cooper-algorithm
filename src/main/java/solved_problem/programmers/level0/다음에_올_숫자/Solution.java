2944431a091f61e6f676ee7a8e2e27be9b127962
cooper <pbg0205@naver.com>
2023-03-11 23:06:03 +0900

    [programmers] 다음에 올 숫자

---
+++ src/main/java/solved_problem/programmers/level0/다음에_올_숫자/Solution.java
@@ -0,0 +1,28 @@
+package solved_problem.programmers.level0.다음에_올_숫자;
+
+class Solution {
+    public int solution(int[] common) {
+        int lastIndex = common.length - 1;
+
+        if (isArithmeticSequence(common[0], common[1], common[2])) {
+            int degree = common[1] - common[0];
+            return common[lastIndex] + degree;
+        }
+
+        if (isGeometricSequence(common[0], common[1], common[2])) {
+            int ratio = common[1] / common[0];
+            return common[lastIndex] * ratio;
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    private boolean isArithmeticSequence(int a, int b, int c) {
+        return 2 * b == a + c;
+    }
+
+    private boolean isGeometricSequence(int a, int b, int c) {
+        return (b * b) == (a * c);
+    }
+
+}
