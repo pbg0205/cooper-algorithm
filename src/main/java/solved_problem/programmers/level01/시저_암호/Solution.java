fe73740cb0ae13b81b6d9fee6bbafbf4f72293cd
cooper <pbg0205@naver.com>
2023-04-14 23:39:38 +0900

    [programmers] 시저 암호

---
+++ src/main/java/solved_problem/programmers/level01/시저_암호/Solution.java
@@ -0,0 +1,28 @@
+package solved_problem.programmers.level01.시저_암호;
+
+class Solution {
+
+    private static final int ALPHABET_SIZE = 26;
+
+    public String solution(String s, int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            stringBuilder.append(push(c, n));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private char push(char c, int n) {
+        if (!Character.isAlphabetic(c)) {
+            return c;
+        }
+
+        int offset = Character.isUpperCase(c) ? 'A' : 'a';
+        int position = c - offset;
+        position = (position + n) % ALPHABET_SIZE;
+        return (char) (position + offset);
+    }
+
+}
