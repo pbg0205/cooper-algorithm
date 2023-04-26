59a0233ef4f6c14de0cf350976a6615a97d52dc8
cooper <pbg0205@naver.com>
2023-04-26 21:32:15 +0900

    [programmers] 쿼드 압축 후 개수 세기

---
+++ src/main/java/solved_problem/programmers/level02/쿼드_압축_후_개수_세기/Solution.java
@@ -0,0 +1,46 @@
+package solved_problem.programmers.level02.쿼드_압축_후_개수_세기;
+
+class Solution {
+
+    private static class Count {
+        private final int zeroCount;
+        private final int oneCount;
+
+        public Count(int zeroCount, int oneCount) {
+            this.zeroCount = zeroCount;
+            this.oneCount = oneCount;
+        }
+
+        public Count add(Count other) {
+            return new Count(this.zeroCount + other.zeroCount, this.oneCount + other.oneCount);
+        }
+    }
+
+    public int[] solution(int[][] arr) {
+        Count count = count(0, 0, arr.length, arr);
+
+        return new int[]{count.zeroCount, count.oneCount};
+    }
+
+    private Count count(int offsetY, int offsetX, int size, int[][] arr) {
+        int h = size / 2;
+
+        for (int y = offsetY; y < offsetY + size; y++) {
+            for (int x = offsetX; x < offsetX + size; x++) {
+                if (arr[y][x] != arr[offsetY][offsetX]) {
+                    return count(offsetY, offsetX, h, arr)
+                            .add(count(offsetY + h, offsetX, h, arr))
+                            .add(count(offsetY, offsetX + h, h, arr))
+                            .add(count(offsetY + h, offsetX + h, h, arr));
+                }
+            }
+        }
+
+        if (arr[offsetY][offsetX] == 1) {
+            return new Count(0, 1);
+        }
+
+        return new Count(1, 0);
+    }
+
+}
