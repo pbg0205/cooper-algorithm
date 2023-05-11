bc5b9c093f0d7564ad6d84437ccb07e5d8c8be75
cooper <pbg0205@naver.com>
2023-05-11 20:19:09 +0900

    [programmers] 체육복

---
+++ src/main/java/solved_problem/programmers/level01/체육복/Solution2.java
@@ -0,0 +1,61 @@
+package solved_problem.programmers.level01.체육복;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class Solution2 {
+
+    public int solution(int n, int[] lost, int[] reserve) {
+        /**
+         * 정렬을 해야 하는 케이스 : 분실 2인 [3,1] / 여분 2인 [2,4]
+         * - 2번이 3번에게 체육복을 빌려주면 4번은 1번에게 체육복을 빌려줄 수 없는 예외가 발생한다.
+         */
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        Set<Integer> owns = Arrays.stream(lost)
+                .boxed()
+                .collect(Collectors.toSet());
+
+        owns.retainAll(Arrays.stream(reserve)
+                .boxed()
+                .collect(Collectors.toSet()));
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int lostNumber : lost) {
+            queue.add(lostNumber);
+        }
+
+        int count = 0;
+        for (int reserveNumber : reserve) {
+            if (owns.contains(reserveNumber)) {
+                continue;
+            }
+
+            while (!queue.isEmpty() && (queue.peek() < reserveNumber - 1 || owns.contains(queue.peek()))) {
+                queue.poll();
+            }
+
+            if (queue.isEmpty()) {
+                break;
+            }
+
+            if (queue.peek() <= reserveNumber + 1) {
+                queue.poll();
+                count++;
+            }
+        }
+
+        return n - lost.length + owns.size() + count;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(5, new int[]{2, 4}, new int[]{1, 3, 5});
+    }
+
+}
