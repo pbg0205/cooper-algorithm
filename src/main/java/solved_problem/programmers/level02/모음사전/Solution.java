5c060bea42209def426e46dccfde14ba582c7ecc
cooper <pbg0205@naver.com>
2023-06-30 10:48:32 +0900

    [programmers] 모음사전

--- src/main/java/solved_problem/programmers/level02/모음사전/Solution.java
+++ src/main/java/solved_problem/programmers/level02/모음사전/Solution.java
@@ -3,6 +3,9 @@ package solved_problem.programmers.level02.모음사전;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * word 값을 하나씩 추가하면서 재귀 함수를 통해 넘기는 문제
+ */
 class Solution {
 
     private static final char[] CHARS = "AEIOU".toCharArray();
