028a8546d3a83eae1e3888841f28d4df87fc7862
cooper <pbg0205@naver.com>
2023-07-03 02:49:53 +0900

    [programmers] 문자열 내림차순 배치하기

--- src/main/java/solved_problem/programmers/level01/문자열_내림차순_배치하기/Solution.java
+++ src/main/java/solved_problem/programmers/level01/문자열_내림차순_배치하기/Solution.java
@@ -1,16 +1,19 @@
 package solved_problem.programmers.level01.문자열_내림차순_배치하기;
 
+import java.util.Arrays;
+
 class Solution {
 
     public String solution(String s) {
-        return s.chars() // ascii-code ('A' : 65, 'a' : 97)
-                .boxed()
-                .sorted((v1, v2) -> v2 - v1)
-                .collect(
-                        StringBuilder::new,
-                        StringBuilder::appendCodePoint, //ascii-code 의 character 를 추가한다.
-                        StringBuilder::append)
-                .toString();
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+
+        StringBuilder builder = new StringBuilder();
+        for (int idx = chars.length - 1; idx >= 0; idx--) {
+            builder.append(chars[idx]);
+        }
+
+        return builder.toString();
     }
 
 }
