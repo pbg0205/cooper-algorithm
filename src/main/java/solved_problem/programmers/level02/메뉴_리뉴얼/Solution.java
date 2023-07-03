4fcde8bc3d33d72a83e06bd5bc90e6cbee3f70dd
cooper <pbg0205@naver.com>
2023-07-03 22:13:16 +0900

    [programmers] 메뉴 리뉴얼

--- src/main/java/solved_problem/programmers/level02/메뉴_리뉴얼/Solution.java
+++ src/main/java/solved_problem/programmers/level02/메뉴_리뉴얼/Solution.java
@@ -2,115 +2,86 @@ package solved_problem.programmers.level02.메뉴_리뉴얼;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 class Solution {
 
-    public String[] solution(String[] orders, int[] course) {
-        List<Set<String>> orderList = Arrays.stream(orders)
-                .map(String::chars)
-                .map(charStream -> charStream
-                        .mapToObj(menu -> String.valueOf((char) menu))
-                        .collect(Collectors.toSet()))
-                .collect(Collectors.toList());
+    public List<String> solution(String[] orders, int[] course) {
+        Map<Integer, Map<String, Integer>> menuCountMap = initCourseCountMap(course);
 
-        for (Set<String> set : orderList) {
-            System.out.println(set.toString());
+        // 1. 코스의 메뉴 구성 수에 맞는 코스 후보군을 조합(combination) 을 통해 구한다. (by. backTracking)
+        for (String order : orders) {
+            for (int courseCount : course) {
+                if (order.length() < courseCount) {
+                    continue;
+                }
+
+                String[] orderMenu = order.split("");
+                Arrays.sort(orderMenu);
+                combination(orderMenu, new String[courseCount], 0, 0, courseCount,
+                        new boolean[order.length()], menuCountMap);
+            }
         }
 
-        Map<Integer, List<Course>> courses = new HashMap<>();
-        for (int length : course) {
-            List<Course> list = new ArrayList<>();
-            list.add(new Course("", 0));
-            courses.put(length, list);
-        }
+        // 2. 같은 메뉴 구성 수의 후보군 중, 주문 수가 가장 많은 코스를 선정한다.
+        List<String> answerList = new ArrayList<>();
+        for (int eachCourseCount : course) {
+            Map<String, Integer> eachCourseCountMap = menuCountMap.get(eachCourseCount);
+            List<Integer> courseCounts = new ArrayList<>(eachCourseCountMap.values());
+            int maxCount = Integer.MIN_VALUE;
 
-        System.out.println(courses.toString());
-
-        getCourses('A', new HashSet<>(), orderList, courses);
-
-        System.out.println(courses.toString());
-
-        return courses.values().stream()
-                .filter(list -> list.get(0).occurrences > 0)
-                .flatMap(List::stream)
-                .map(c -> c.course)
-                .sorted()
-                .toArray(String[]::new);
-    }
-
-    private void getCourses(char nextMenu,
-                            Set<String> selectedMenus,
-                            List<Set<String>> orderList,
-                            Map<Integer, List<Course>> courses) {
-
-        System.out.println(nextMenu);
-        System.out.println(selectedMenus);
-        System.out.println(orderList);
-        System.out.println(courses);
-        System.out.println("------------------");
-
-        int occurrences = (int) orderList.stream()
-                .filter(order -> order.containsAll(selectedMenus))
-                .count();
-
-        if (occurrences < 2) {
-            return;
-        }
-
-        int size = selectedMenus.size();
-        if (courses.containsKey(size)) {
-            List<Course> courseList = courses.get(size);
-            String courseStr = selectedMenus.stream()
-                    .sorted()
-                    .collect(Collectors.joining(""));
-
-            Course course = new Course(courseStr, occurrences);
-
-            Course original = courseList.get(0);
-
-            if (original.occurrences < occurrences) {
-                courseList.clear();
-                courseList.add(course);
-            } else if (original.occurrences == occurrences) {
-                courseList.add(course);
+            for (int courseCount : courseCounts) {
+                maxCount = Math.max(maxCount, courseCount);
             }
 
+            for (Map.Entry<String, Integer> eachCourseCountEntry : eachCourseCountMap.entrySet()) {
+                String courseMenu = eachCourseCountEntry.getKey();
+                int selectedCount = eachCourseCountEntry.getValue();
+
+                if (selectedCount == maxCount && selectedCount >= 2) {
+                    answerList.add(courseMenu);
+                }
+            }
         }
 
-        if (size >= 10) {
+        Collections.sort(answerList);
+
+        return answerList;
+    }
+
+    private static Map<Integer, Map<String, Integer>> initCourseCountMap(int[] course) {
+        Map<Integer, Map<String, Integer>> courseCountMap = new HashMap<>();
+        for (int eachCourse : course) {
+            courseCountMap.put(eachCourse, new HashMap<>());
+        }
+        return courseCountMap;
+    }
+
+    private void combination(String[] orderMenu,
+                             String[] courseCandidate,
+                             int depth,
+                             int startIdx,
+                             int courseDepth,
+                             boolean[] visited,
+                             Map<Integer, Map<String, Integer>> menuCountMap) {
+        if (depth == courseDepth) {
+            Map<String, Integer> courseCountMap = menuCountMap.get(courseDepth);
+            String candidateMenu = String.join("", courseCandidate);
+            int courseCount = courseCountMap.getOrDefault(candidateMenu, 0);
+            courseCountMap.put(candidateMenu, courseCount + 1);
             return;
         }
 
-        for (char menuChar = nextMenu; menuChar <= 'Z'; menuChar++) {
-            String menu = String.valueOf(menuChar);
-            selectedMenus.add(menu);
-            getCourses((char) (menuChar + 1), selectedMenus, orderList, courses);
-            selectedMenus.remove(menu);
+        for (int idx = startIdx; idx < orderMenu.length; idx++) {
+            if (!visited[idx]) {
+                visited[idx] = true;
+                courseCandidate[depth] = orderMenu[idx];
+                combination(orderMenu, courseCandidate, depth + 1, idx + 1, courseDepth, visited, menuCountMap);
+                visited[idx] = false;
+            }
         }
     }
-
-    private static class Course {
-        public final String course;
-        public final int occurrences;
-
-        public Course(String course, int occurrences) {
-            this.course = course;
-            this.occurrences = occurrences;
-        }
-
-        @Override
-        public String toString() {
-            return "Course{" +
-                    "course='" + course + '\'' +
-                    ", occurrences=" + occurrences +
-                    '}';
-        }
-    }
-
 }
--- src/test/java/solved_problem/programmers/level02/메뉴_리뉴얼/SolutionTest.java
+++ src/test/java/solved_problem/programmers/level02/메뉴_리뉴얼/SolutionTest.java
@@ -1,6 +1,11 @@
 package solved_problem.programmers.level02.메뉴_리뉴얼;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,16 +13,28 @@ class SolutionTest {
 
     private Solution solution = new Solution();
 
-    @Test
-    void test() {
-        //given
-        String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
-        int[] courses = new int[]{2, 3, 4};
+    public static Stream<Arguments> testSource() {
+        return Stream.of(
+                Arguments.of(
+                        new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"},
+                        new int[]{2, 3, 4},
+                        List.of("AC", "ACDE", "BCFG", "CDE")
+                )
+                , Arguments.of(
+                        new String[]{"ABCDE", "ABCDE", "ABCDE", "ABCDE", "ABCDE", "ABCDE", "ABCDE"},
+                        new int[]{2, 3, 5},
+                        List.of("AB", "ABC", "ABCDE", "ABD", "ABE", "AC", "ACD", "ACE", "AD", "ADE", "AE", "BC", "BCD", "BCE", "BD", "BDE", "BE", "CD", "CDE", "CE", "DE"))
+        );
+    }
 
-        //when
-        String[] actual = solution.solution(orders, courses);
+    @MethodSource("testSource")
+    @ParameterizedTest
+    void test(String[] orders, int[] course, List<String> result) {
+        //given, when
+        List<String> actual = solution.solution(orders, course);
 
-        assertThat(actual).isEqualTo(new String[]{"AC", "ACDE", "BCFG", "CDE"});
+        //then
+        assertThat(actual).isEqualTo(result);
     }
 
 }
