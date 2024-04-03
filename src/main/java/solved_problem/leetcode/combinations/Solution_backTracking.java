ee075d9bd0a5001502ad5725088d7623e5d277f5
cooper <pbg0205@naver.com>
2024-04-03 21:54:39 +0900

    [leetcode] combinations

--- src/main/java/solved_problem/leetcode/combinations/Solution_backTracking.java
+++ src/main/java/solved_problem/leetcode/combinations/Solution_backTracking.java
@@ -5,21 +5,13 @@ import java.util.List;
 
 class Solution_backTracking {
 
-    boolean[] visited;
-    List<List<Integer>> result;
-    int m;
-
     public List<List<Integer>> combine(int n, int k) {
-        result = new ArrayList<>();
-        visited = new boolean[n];
-
-        m = k;
-        backTracking(n, k, 1, new ArrayList<>());
-
+        List<List<Integer>> result = new ArrayList<>();
+        bfs(n, k, 1, new ArrayList<>(), result);
         return result;
     }
 
-    private void backTracking(int n, int k, int start, List<Integer> list) {
+    private void bfs(int n, int k, int start, List<Integer> list, final List<List<Integer>> result) {
         if (list.size() == k) {
             result.add(new ArrayList<>(list));
             return;
@@ -27,7 +19,7 @@ class Solution_backTracking {
 
         for (int i = start; i <= n; i++) {
             list.add(i);
-            backTracking(n, k, i + 1, list);
+            bfs(n, k, i + 1, list, result);
             list.remove(list.size() - 1);
         }
     }
