3e4ae3a936ec2acb011b9726bd91485987ecbb8f
cooper <pbg0205@naver.com>
2024-04-14 22:03:18 +0900

    [programmers] 입국심사

--- src/main/java/solved_problem/programmers/level03/입국검사/Solution2.java
+++ src/main/java/solved_problem/programmers/level03/입국검사/Solution2.java
@@ -3,34 +3,39 @@ package solved_problem.programmers.level03.입국검사;
 import java.util.Arrays;
 
 class Solution2 {
+	public long solution(int n, int[] times) {
+		long answer = 0;
 
-    public long solution(int n, int[] times) {
-        Arrays.sort(times);
+		long timeMin = 1; // 입국 시간 최소 값
+		long timeMax = (long) Arrays.stream(times).max().getAsInt() * n; // 입국 시간 최대 값
+		long timeMid = timeMax; // 입국 시간 중간 값
 
-        long min = 1;
-        long max = (long) times[times.length - 1] * n;
+		while(timeMin <= timeMax) {
+			System.out.println("timeMin = " + timeMin);
+			System.out.println("timeMax = " + timeMax);
+			System.out.println("timeMid = " + timeMid);
 
-        while (min <= max) {
-            long sum = 0L; // 입국 검사한 사람 수
-            long mid = min + (max - min) / 2; // 소요 시간
+			long immigrationCount = 0; // 특정 시간의 입국 가능 사람 수
+			for (int timePerOfficer : times) {
+				immigrationCount += timeMid / timePerOfficer;
+			}
 
-            for (int time : times) {
-                sum += mid / time;
-            }
+			if (immigrationCount >= n) {
+				timeMax = timeMid - 1;
+				answer = timeMid; // n 명이 가능한 최소 값을 구하기 위한 로직
+			} else {
+				timeMin = timeMid + 1;
+			}
 
-            if (sum >= n) {
-                max = mid - 1;
-            } else {
-                min = mid + 1;
-            }
-        }
+			timeMid = timeMin + (timeMax - timeMin) / 2;
+		}
 
-        return min;
-    }
+		return answer;
+	}
 
-    public static void main(String[] args) {
-        Solution2 solution2 = new Solution2();
-        long result = solution2.solution(6, new int[]{7, 10});
-        System.out.println("result = " + result);
-    }
+	public static void main(String[] args) {
+		Solution2 solution = new Solution2();
+		long result = solution.solution(6, new int[] {7, 10});
+		System.out.println("result = " + result);
+	}
 }
