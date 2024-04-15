7b7fcdb2d6f9a85bbad356422d052ccee965f11c
cooper <pbg0205@naver.com>
2024-04-16 00:01:52 +0900

    quicksort summary

--- src/main/java/summary/sorting/QuickSort.java
+++ src/main/java/summary/sorting/QuickSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class QuickSort {
 	public static void main(String[] args) {
-		int[] numbers = new int[]{3, 4, 5, 6, 1, 2, 10, 9, 8, 7};
+		int[] numbers = new int[]{3, 4, 6, 5, 1, 2, 10, 9, 8, 7};
 		quickSort(numbers, 0, numbers.length - 1);
 
 		System.out.println(Arrays.toString(numbers));
@@ -13,6 +13,12 @@ public class QuickSort {
 	private static void quickSort(final int[] numbers, final int low, final int high) {
 		if (low < high) {
 			int pivot = partition(numbers, low, high);
+
+			System.out.println(Arrays.toString(numbers));
+			System.out.println("low = " + low);
+			System.out.println("high = " + high);
+			System.out.println("pivot = " + pivot);
+
 			quickSort(numbers, low, pivot - 1);
 			quickSort(numbers, pivot + 1, high);
 		}
@@ -28,11 +34,7 @@ public class QuickSort {
 				left++;
 			}
 		}
-
 		swap(numbers, left, high);
-
-		System.out.println(Arrays.toString(numbers));
-
 		return left;
 	}
 
