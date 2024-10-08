b3be2fb4d44ec565139b1c198ffaf1c40be750e4
cooper <pbg0205@naver.com>
2024-10-08 23:31:28 +0200

    [programmers] 명예의 전당

---
+++ src/main/java/solved_problem/programmers/level01/명예의_전당/Solution.java
@@ -0,0 +1,24 @@
+package solved_problem.programmers.level01.명예의_전당;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+	public int[] solution(int k, int[] score) {
+		int[] answer = new int[score.length];
+
+		List<Integer> rank = new ArrayList<>();
+
+		for (int i = 0; i < score.length; i++) {
+			rank.add(score[i]);
+
+			if (rank.size() > k) {
+				rank.remove(Collections.min(rank));
+			}
+
+			answer[i] = Collections.min(rank);
+		}
+		return answer;
+	}
+}
